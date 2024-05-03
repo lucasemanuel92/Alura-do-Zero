a73d4c5978515a97d44b73ffb8af46d3166fbeef
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2024-05-03 18:49:53 -0300

    Add files via upload

---
+++ Mergulhe/desafio1.js
@@ -0,0 +1,29 @@
+// Criar uma funcao que exibe ola mundo
+function exibirOla(){
+    console.log(`ola mundo`);
+}
+
+exibirOla();
+
+// Criar uma funcao que recebe um nome e imprime Ola, [nome]!
+function imprimirNome(nome) {
+    console.log(`Ola, ${nome}`)
+}
+
+imprimirNome(`Lucas`); 
+
+// Recebe um numero e retorna o dobro
+function dobro(numero) {
+    return numero * 2;
+} 
+
+let resultado = dobro(5);
+console.log(resultado);
+
+// Recebe 3 numeros e calcula a media
+function calcularMedia(a, b, c) {
+    return (a + b + c) / 3;
+}
+
+let media = calcularMedia(3, 5, 6);
+console.log(media);
