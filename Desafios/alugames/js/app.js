7f054736f2745eb7f2b87a73b971fe3d047a8343
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-08-16 12:42:44 -0300

    alugames part 2

--- Desafios/alugames/js/app.js
+++ Desafios/alugames/js/app.js
@@ -3,8 +3,19 @@
 function alterarStatus(id) {
     let gameSelecionado = document.getElementById(`game-${id}`);
     let imagem = gameSelecionado.querySelector('.dashboard__item__img');
-    let botão = gameSelecionado.querySelector('.dashboard__item__button');
-    let nomeJogo = gameSelecionado.querySelector('.dashboard__item__name');
+    let botao = gameSelecionado.querySelector('.dashboard__item__button');
+    // let nomeJogo = gameSelecionado.querySelector('.dashboard__item__name'); Usado no teste
 
-    alert(nomeJogo.innerHTML); // Posso usar o inner html
+    // Dentro do if temos (variável.chamarListaDeClasses.temEssaClasse('.nome__da__classe'))
+    if (imagem.classList.contains('.dashboard__item__img--rented')) {
+        imagem.classList.remove('.dashboard__item__img--rented');
+        botao.classList.remove('.dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('.dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.remove('.dashboard__item__button--return');
+    }
+
+    // alert(nomeJogo.textContent); // Posso usar o ".innerHTML" // Teste para saber se estava funcionando
 }
