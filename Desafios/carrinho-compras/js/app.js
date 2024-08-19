8f685b81218ec00a0aedc1b38cdf6bebfbe79f1b
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-08-19 10:36:45 -0300

    Parte 2 - Split

--- Desafios/carrinho-compras/js/app.js
+++ Desafios/carrinho-compras/js/app.js
@@ -2,11 +2,17 @@
 
 function adicionar() {
     // Recuperar os valores: Nome/quantidade/valor
-    let produto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    // O ".split()" é moldado pelo elemento que se quer separar e a posição que ele se encontra (é um array)
+    let nomeProduto = produto.split('-')[0]; // Antes do split
+    let valorUnitario = produto.split('R$')[1]; // Depois do split
     let quantidade = document.getElementById('quantidade');
 
-    alert(produto.value);
-    alert(quantidade.value)
+    let subtotal = quantidade.value * valorUnitario;
+
+    alert(subtotal)
+
+    
     // Calcular o preço por produto (subtotal) = valor unidade * quantidade 
     // Adicionar ao carrinho
     // Soma dos subtotais
