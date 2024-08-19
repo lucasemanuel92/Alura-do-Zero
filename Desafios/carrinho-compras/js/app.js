6338de306e85392b5880abccf01cf003abf572d6
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-08-19 10:44:50 -0300

    Parte 3 - Mudança na parte HTML

--- Desafios/carrinho-compras/js/app.js
+++ Desafios/carrinho-compras/js/app.js
@@ -6,15 +6,18 @@ function adicionar() {
     // O ".split()" é moldado pelo elemento que se quer separar e a posição que ele se encontra (é um array)
     let nomeProduto = produto.split('-')[0]; // Antes do split
     let valorUnitario = produto.split('R$')[1]; // Depois do split
-    let quantidade = document.getElementById('quantidade');
-
-    let subtotal = quantidade.value * valorUnitario;
-
-    alert(subtotal)
-
+    let quantidade = document.getElementById('quantidade').value;
     
     // Calcular o preço por produto (subtotal) = valor unidade * quantidade 
+    let subtotal = quantidade * valorUnitario;
+
     // Adicionar ao carrinho
+    // A tag section é o que adiciona o elemento no carrinho
+    let listaCarrinho = document.getElementById('lista-produtos');
+    listaCarrinho.innerHTML = listaCarrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${valorUnitario}</span>
+        </section>`
+
     // Soma dos subtotais
 }
 
