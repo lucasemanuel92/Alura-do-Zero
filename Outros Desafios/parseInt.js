9990a58e8fcd5f49a5539e558f5b21c55f45f7fc
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-08-21 14:20:33 -0300

    Desafio parseInt

---
+++ Outros Desafios/parseInt.js
@@ -0,0 +1,8 @@
+// Criar uma função que receba uma string, converta em inteiro e retorne o resultado
+
+function conversao(texto) {
+    return parseInt(texto)
+}
+let texto = '1992';
+let final = conversao(texto)
+console.log(final);
