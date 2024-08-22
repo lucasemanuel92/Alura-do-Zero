2e619cad9e9ffb3b2eec51a64f9ae6f3b43c113d
Lucas <lucas.emanuel.carvalho@outlook.com>
2024-08-22 12:40:24 -0300

    Maior/Menor idade

---
+++ Desafios Finais/maiorIdade.js
@@ -0,0 +1,18 @@
+// Verificar se uma pessoa é maior de idade
+function maiorIdade(idade) {
+    if (idade >= 18) {
+        return 'A pessoa é maior de idade'
+    } else {
+        return 'A pessoa é menor de idade'
+    }
+}
+
+console.log(maiorIdade(21))
+
+// Forma mais curta
+function verificarIdade(idade){
+    // return condição if : else
+    return idade >= 18 ? 'Maior de idade' : 'Menor de idade'
+}
+
+console.log(verificarIdade(9))
